dc7d86bbe920ef5f7ab01b52701ad26a2f2d7000
Peter Oberlaender <pete@wayoutofline.net>
2023-09-04 22:34:44 -0400

    Create index.php
    Just some basic stuff / WARNING: no CSS!

---
+++ index.php
@@ -0,0 +1,140 @@
+<?php
+//// Displaying errors
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
+    //// Load up the DB connection
+    require_once('db/db.php');
+
+//// Setting up users -> was now done in DB...
+    // $user1 = "peter";
+    // $passw1 = "pizza123";
+    // $user2 = "mary";
+    // $passw2 = "pasta123";
+
+
+//// POST check & content check via if conditional...
+    if(isset($_POST["username"]) && !empty($_POST["username"]) && $_POST["username"] != " ") {
+        
+        /// Assigning values to variables as well as "sanitizing" the inputs
+        $username = trim(strip_tags($_POST["username"]));
+        $password = trim(strip_tags($_POST["password"]));
+
+        /// DB SELECT OPERATION
+        $user = $db->real_escape_string($username);
+        $passw = $db->real_escape_string($password);
+
+        $query = "SELECT * FROM zjhu5_users WHERE username = '$username'";
+        $result = mysqli_query($db, $query);
+        
+        if(mysqli_num_rows($result) == 0) {
+            echo("<p>user not found</p>");
+            exit();
+        }
+
+        $user_data = mysqli_fetch_assoc($result);
+
+        echo("<pre>");
+        var_dump($user_data);
+        echo("</pre>");
+
+
+
+
+        //echo "Form data is OK";
+        //if(($username === $user1 && $passw1 === $password) || ($username === $user2 && $passw2 === $password)) {
+        //if($password == $user_data['password']) -> won't work, because $password isn't hashed yet
+        if(password_verify($password, $user_data['password'])) {
+            session_start();
+            $_SESSION["username"] = $username;
+            $_SESSION["id"] = $user_data['id'];
+            //Boolean switch type variable
+            $_SESSION["logged_in"] = true;
+
+            print "Hi there, ".$_SESSION["username"];
+        }
+    }
+
+    // if(isset($_GET["logout"])) {
+    //     session_start();
+    //     session_destroy();
+    //     echo 'You have been logged out. <a href="login.php">Go back</a>';
+    // }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Simple Login</title>
+</head>
+<body>
+
+
+    <?php if(isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) : ?>
+    
+        <h1>Welcome <?php print $_SESSION["username"]; ?></h1>
+
+        <p id="messages">l o a d i n g</p>
+
+        <form id="msg-box" action="views/chat.php?post" method="post">
+            <input type="text" placeholder="type a message" name="msg">
+            <input type="submit" value="Post it!">
+        </form>
+
+        <script>
+
+            const myMessages = document.querySelector("#messages");
+
+            //let msgData; 
+            function loadMessages() {
+                fetch("views/chat.php?loadAll")
+                .then((result) => result.json())
+                .then((data) => {
+                    console.log(data);
+                    //msgData = data;
+                    myMessages.innerHTML = "";
+
+                    data.forEach(element => {
+                        let msgBox = `
+                            <span class="msg">
+                                ${element.message}<br>
+                                <i>${element.uname}</i>
+                            </span>
+                            <br>
+                        `;
+                        myMessages.innerHTML += msgBox;
+                    });
+
+                    
+
+                });
+            }
+            loadMessages();
+        </script>
+
+        <a href="logout.php">Log me out</a>
+    
+    <?php elseif($_SERVER['REQUEST_METHOD'] == "POST") : ?>
+
+        <h2>oops, that didn't work... </h2>
+        <a href="./index.php">Try again.</a>
+        
+    <?php else : ?>
+
+        <h1>Please log in first</h1>
+        <form action="./index.php" method="post">
+            <input type="text" name="username" placeholder="User name please" pattern=".{3,}" required>
+            <input type="password" name="password" placeholder="Password please" pattern=".{8,}" required>
+            <input type="submit" value="Log me in!">
+        </form>
+    
+    <?php endif; ?>
+
+    
+
+</body>
+</html>
